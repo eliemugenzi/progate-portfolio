f58deed7c67a4220c40b79cd78ec98d20f9f8f48
Elie Mugenzi <eliemugenzi@Elies-MacBook-Pro.local>
2019-06-01 16:43:26 +0200

    Updates

---
+++ precache-manifest.6d651f1b3e3b8e0c02f2890c834c9e81.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba86d79eac09c75e455a3f31da640b3",
+    "url": "/progate-portfolio/index.html"
+  },
+  {
+    "revision": "0eae99bf866d1f9570c8",
+    "url": "/progate-portfolio/static/css/2.b4b23ec4.chunk.css"
+  },
+  {
+    "revision": "597f4221af00c68b31e5",
+    "url": "/progate-portfolio/static/css/main.81056636.chunk.css"
+  },
+  {
+    "revision": "0eae99bf866d1f9570c8",
+    "url": "/progate-portfolio/static/js/2.fa9683e8.chunk.js"
+  },
+  {
+    "revision": "597f4221af00c68b31e5",
+    "url": "/progate-portfolio/static/js/main.3eda53c5.chunk.js"
+  },
+  {
+    "revision": "7228881d21e17677c62c",
+    "url": "/progate-portfolio/static/js/runtime~main.a1a73be7.js"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/progate-portfolio/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/progate-portfolio/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/progate-portfolio/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/progate-portfolio/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/progate-portfolio/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/progate-portfolio/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/progate-portfolio/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/progate-portfolio/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/progate-portfolio/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/progate-portfolio/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/progate-portfolio/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/progate-portfolio/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/progate-portfolio/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/progate-portfolio/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/progate-portfolio/static/media/fa-solid-900.f14c3b2f.ttf"
+  }
+]);
